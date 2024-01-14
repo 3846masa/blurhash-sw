04a9c636a9aa7b51a95eeefe741c6c456e218223
3846masa <3846masahiro+git@gmail.com>
2024-01-14 17:11:17 +0900

    chore: add delay mode in demo

--- demo/public/sw.js
+++ demo/public/sw.js
@@ -13,3 +13,23 @@ self.addEventListener('install', (ev) => {
 self.addEventListener('activate', (ev) => {
   ev.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('fetch', (/** @type {FetchEvent} */ ev) => {
+  const url = new URL(ev.request.url);
+  const delay = parseInt(url.searchParams.get('__delay__'));
+
+  if (Number.isFinite(delay)) {
+    return ev.respondWith(
+      Promise.all([
+        fetch(ev.request, { cache: 'no-store', mode: 'cors', credentials: 'omit' }).then((res) => {
+          return new Response(res.body, {
+            headers: new Headers([...res.headers, ['Cache-Control', 'no-store']]),
+          });
+        }),
+        new Promise((resolve) => setTimeout(resolve, delay)),
+      ]).then(([res]) => res),
+    );
+  } else {
+    return;
+  }
+});
